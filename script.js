f51f7aaa10edd0a903d176c16d064dfd34f79789
TetianaHazzaieva <gorodned5@gmail.com>
2025-07-17 02:08:19 +0000

    bug fixes

--- script.js
+++ script.js
@@ -1,63 +1,51 @@
-const butterflies = ['img/butterfly-blue.png', 'img/butterfly-colorful.png', 'img/butterfly-multicolored.png', 'img/butterfly-red.png', 'img/butterfly-yellow.png'];
-const gameField = document.getElementById('game-field');
-const scoreEl = document.getElementById('score');
-const startBtn = document.getElementById('start-btn');
-const difficultySelect = document.getElementById('difficulty');
+const butterflies = [
+  "./img/butterfly-blue.png",
+  "./img/butterfly-colorful.png",
+  "./img/butterfly-multicolored.png",
+  "./img/butterfly-red.png",
+  "./img/butterfly-yellow.png"
+];
+const gameField = document.getElementById("game-field");
+const scoreEl = document.getElementById("score");
+const startBtn = document.getElementById("start-btn");
+const difficultySelect = document.getElementById("difficulty");
 
 let score = 0;
 let gameInterval;
 let butterflySpeed = 3000;
 
-startBtn.addEventListener('click', startGame);
+startBtn.addEventListener("click", startGame);
 
 function startGame() {
-    score = 0;
-    updateScore(0);
-    gameField.innerHTML = '';
+  clearInterval(gameInterval);
+  gameField.innerHTML = "";
+  score = 0;
+  updateScore();
+
+const speed = 
+difficultySelect.value === 'medium' ? 2000 :
+difficultySelect.value === 'hard' ? 1000 : 3000;
+
+gameInterval = setInterval(() => spawnButterfly(speed), speed);
 }
 
-const difficulty = difficultySelect.ariaValueMax;
-switch(difficulty) {
-    case 'easy':
-        butterflySpeed = 3000;
-        break;
-        case 'medium':
-            butterflySpeed = 2000;
-            break;
-            case 'hurd':
-                butterflySpeed = 1000;
-                break;
+function spawnButterfly(lifeTime) {
+  const img = document.createElement("img");
+  img.src = butterflies[Math.floor(Math.random() * butterflies.length)];
+  img.className = "butterfly";
+
+  const rect = gameField.getBoundingClientRect();
+  img.style.left = `${Math.random() * (rect.Width - 60)}px`;
+  img.style.top = `${Math.random() * (rect.Height - 60)}px`;
+
+  img.onklick = () => { img.remove(), updateScore(score + 1);
+  gameField.appendChild(img);
+  
+  setTimeout(() => lifeTime * 0.9);
 }
 
-if (gameInterval) clearInterval(gameInterval);
-
-gameInterval = setInterval(spawnButterfly, butterflySpeed);
-
-function spawnButterfly() {
-    const butterfly = document.createElement('img');
-    butterfly.src = butterflies[Math.floor(Math.random() * butterflies.length)];
-    butterfly.classList.add('butterfly');
-
-    const fieldRect = gameField.getClientRect();
-    const x = Math.random() * (gameField.clientWidth - 60);
-    const y = Math.random() * (gameField.clientHeight - 60);
-
-    butterfly.style.left = `${x}px`;
-    butterfly.style.top = `${y}px`;
-
-    butterfly.addEventListener('click', () => {
-        butterfly.remove();
-        updateScore(score + 1);
-    });
-
-    gameField.appendChild(butterfly);
-
-    setTimeout(() => {
-        butterfly.remove();
-    }, butterflySpeed - 100);
+function updateScore(val = score) {
+  score = val;
+  scoreEl.textContent = score;
 }
-
-function updateScore(newScore) {
-    score = newScore;
-    scoreEl.textContent = score;
 }
